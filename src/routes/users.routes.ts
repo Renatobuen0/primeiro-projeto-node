cfe1259494a00b725384adbf2f38c62400619f62
Renato Bueno 💻 <renatosapa@gmail.com>
2020-11-22 18:54:36 -0300

    adjust user route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -15,6 +15,17 @@ usersRouter.post('/', async (req, res) => {
       email,
       password,
     });
+
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return res.json(userWithoutPassword);
+
     return res.send(user);
   } catch (error) {
     return res.status(400).json({ error: error.message });
