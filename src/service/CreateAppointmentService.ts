a830f9552d5be7bf41cc64cde7cc8c415d46ad1d
man_technologist: Renato Bueno <renatosapa@gmail.com>
2020-11-22 01:20:35 -0300

    added route users

--- src/service/CreateAppointmentService.ts
+++ src/service/CreateAppointmentService.ts
@@ -1,22 +1,22 @@
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 interface RequestDTO {
   date: Date;
-  provider: string;
+  provider_id: string;
 }
 class CreateAppointmentService {
-  private appotimentsRepository: AppointmentsRepository;
-
-  constructor(appointmentsRepository: AppointmentsRepository) {
-    this.appotimentsRepository = appointmentsRepository;
-  }
-
-  public execute({ date, provider }: RequestDTO): Appointment {
+  public async execute({
+    date,
+    provider_id,
+  }: RequestDTO): Promise<Appointment> {
+    const appotimentsRepository = getCustomRepository(AppointmentsRepository);
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = this.appotimentsRepository.findByDate(
+    const findAppointmentInSameDate = await appotimentsRepository.findByDate(
       appointmentDate,
     );
 
@@ -24,10 +24,13 @@ class CreateAppointmentService {
       throw Error('This appointment is already booked');
     }
 
-    const appointment = this.appotimentsRepository.create({
-      provider,
+    const appointment = appotimentsRepository.create({
+      provider_id,
       date: appointmentDate,
     });
+
+    await appotimentsRepository.save(appointment);
+
     return appointment;
   }
 }
